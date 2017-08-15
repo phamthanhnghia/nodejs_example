229db3fa3491974f19afefa43affb609ca2a5afc
nghia <phamthanhnghia731997@gmail.com>
2017-08-15 13:14:43 +0700

    new2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.static("public"));
 app.set("view engine","ejs");
 app.set("views","./views");
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 app.get("/", function(reg,res){
 	res.render("index");
