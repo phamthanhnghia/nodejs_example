87a56f8bc0d901aa1103f95d0fb67eb2840420cb
nghia <phamthanhnghia731997@gmail.com>
2017-08-17 14:34:58 +0700

    update 2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # nodejs_example
+# socket.io
--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-// var express = require("express");
-// var app = express();
-// app.use(express.static("public"));
-// app.set("view engine","ejs");
-// app.set("views","./views");
-// app.listen(process.env.PORT || 3000);
-//
-// app.get("/", function(reg,res){
-// 	res.render("index");
-// });
 
 var app = require('express')();
 var http = require('http').Server(app);
@@ -17,20 +7,6 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-// io.on('connection', function(socket){
-//   console.log('a user connected');
-//   socket.on('disconnect', function(){
-//     console.log('user disconnected');
-//   });
-// });
-
-
-// io.on('connection', function(socket){
-//   socket.on('chat message', function(msg){
-//     console.log('message: ' + msg);
-//   });
-//   socket.broadcast.emit('hi');
-// });
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
